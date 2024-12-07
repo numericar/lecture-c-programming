10ed05256f2eadeaec0ba3a567d95820cd34cd8b
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2024-12-07 17:11:57 +0700

    first program

--- workspaces/WorkspaceTemplate/Project1/main.c
+++ workspaces/WorkspaceTemplate/Project1/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, char **argv)
-{
-	printf("Default Project\n");
-	return 0;
+int main() {
+    int myFavoriteNumber = 0;
+    
+    printf("Please enter your favorite number\n"); // show text on console
+    scanf("%d", &myFavoriteNumber); // get input from user keyboard
+    printf("Your enterred number: %d\n", myFavoriteNumber);
+    
+    return 0;
 }
