eb5a37581f1dc32450c28ab7532cda30553d70bf
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2024-12-08 10:40:25 +0700

    challenge: displayName

---
+++ challenges/displayName.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    char name[50];
+
+    printf("Enter your username: ");
+    scanf("%s", name);
+    printf("%s is collect.", name);
+    
+    return 0;
+}
