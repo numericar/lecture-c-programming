9c1c506be828f6696fb504847212b8c22de7ab82
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2024-12-10 17:16:34 +0700

    Basic data types in C

---
+++ sources/003_data_type.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() 
+{
+    int a = 100;
+    float b = 100.99;
+    double c = 8.44e+11;
+    // _Bool d = 0; // from original C89
+    bool e = false; // from lib stdbool.h
+
+    return 0;
+}
